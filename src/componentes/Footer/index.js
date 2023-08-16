b21c9f14e66a0ecce1997862fcbd69238f458452
Alejandro <alexxrenteri@gmail.com>
2023-08-16 17:08:43 -0600

    Footer2

--- src/componentes/Footer/index.js
+++ src/componentes/Footer/index.js
@@ -1,7 +1,7 @@
 import "./Footer.css"
 
 const Footer = () => {
-    return <footer className='footer' style={{ backgroundImage:"url/img/footer.png" }}>
+    return <footer className='footer' style={{ backgroundImage:"url(/img/footer.png)"}}>
         <div className='redes'>
             <a href="https://www.facebook.com/alejandro.renteria.35380">
                 <img src="/img/facebook.png" alt="Facebook " />

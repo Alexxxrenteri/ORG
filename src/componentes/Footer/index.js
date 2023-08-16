122d7fd5f539f81cde3bf757ffc16b73723bdbbf
Alejandro <alexxrenteri@gmail.com>
2023-08-16 17:22:58 -0600

    Build2

--- src/componentes/Footer/index.js
+++ src/componentes/Footer/index.js
@@ -1,21 +1,17 @@
 import "./Footer.css"
 
 const Footer = () => {
-    return <footer className='footer' style={{ backgroundImage:"url(/img/footer.png)"}}>
+    return <footer className='footer' style={{ backgroundImage: "url(/img/footer.png)" }}>
         <div className='redes'>
             <a href="https://www.facebook.com/alejandro.renteria.35380">
-                <img src="/img/facebook.png" alt="Facebook " />
+                <img src="/img/facebook.png" alt='Facebook' />
             </a>
             <a href="https://www.instagram.com/alexxrenteri/">
-                <img src="/img/instagram.png" alt=" Instagram " />
+                <img src="/img/instagram.png" alt='instagram' />
             </a>
         </div>
-       <a>
-        <img src="/img/Logo.png" alt="Org" />
-        </a> 
-        <a className="estilo" href="https://github.com/Alexxxrenteri">
-            <h4>Desarrolado por Alejandro</h4>
-        </a>
+        <img src='/img/Logo.png' alt='org' />
+        <strong>Desarrollado por Alejandro</strong>
     </footer>
 }
 
